d464a08be527b4f9f659cb4ce25a7e6d369047c3
Severin Meier <sev.meier@gmail.com>
2017-03-25 22:29:52 +0100

    Improved graph generating script: it can now aggregate values and assign the right number of variables to each data point

--- SatSolverProject/EvaluationGraphScript.py
+++ SatSolverProject/EvaluationGraphScript.py
@@ -1,5 +1,12 @@
 import matplotlib.pyplot as plt
+import matplotlib.ticker as ticker
 import re
+
+inputFile = open('5sat/evaluations.txt')
+AGGREGATE_N = 10
+FILTER_STR = "_unsat"
+# VARIABLES_USED = [3,5,8,10,15,20,25,30,40,50,60,70,80,90,100,120]
+VARIABLES_USED = [3,5,8,10,15,20,25,30,40,50]
 
 def convertToNumeral(inString):
     try:
@@ -7,7 +14,23 @@ def convertToNumeral(inString):
     except:
         return None
 
-inputFile = open('src/test/resources/solving/evaluations.txt')
+def average(inList):
+    av = 0
+    for element in inList:
+        k = convertToNumeral(element)
+        if k != None:
+            av += k
+        else:
+            return None
+    return av / len(inList)
+
+def aggregateResults(inList, n):
+    result = []
+    i = 0
+    while (i < len(inList)):
+        result.append(average(inList[i:i+n]))
+        i += n
+    return result
 
 # Extract information about running the test cases
 line = inputFile.readline()
@@ -24,29 +47,45 @@ cdclTimes = []
 
 for line in inputFile:
     linePattern = re.compile(
-        "dp:(([0-9]*.[0-9*])|x|i)  dpll:(([0-9]*.[0-9*])|x|i)  cdcl:(([0-9]*.[0-9*])|x|i)")
+        "dp:(([0-9]*.[0-9*])|x|i)" +
+        "  dpll:(([0-9]*.[0-9*])|x|i)" +
+        "  cdcl:(([0-9]*.[0-9*])|x|i)" +
+        " \(.*\)")
     lineMatch = linePattern.match(line)
     if lineMatch is None:
         continue
-    dpTimes.append(lineMatch.group(1))
-    dpllTimes.append(lineMatch.group(3))
-    cdclTimes.append(lineMatch.group(5))
+    if FILTER_STR in lineMatch.group(0):
+        dpTimes.append(lineMatch.group(1))
+        dpllTimes.append(lineMatch.group(3))
+        cdclTimes.append(lineMatch.group(5))
 
 
 dpTimes = list(map(convertToNumeral, dpTimes))
 dpllTimes = list(map(convertToNumeral, dpllTimes))
 cdclTimes = list(map(convertToNumeral, cdclTimes))
 
-testList = range(len(dpTimes))
-fig, ax = plt.subplots()
-ax.plot(testList, dpTimes, 'ko', label='DP', mfc='none')
-ax.plot(testList, dpllTimes, 'bx', label='DPLL')
-ax.plot(testList, cdclTimes, 'md', label='CDCL', mfc='none')
+dpTimes = aggregateResults(dpTimes, AGGREGATE_N)
+dpllTimes = aggregateResults(dpllTimes, AGGREGATE_N)
+cdclTimes = aggregateResults(cdclTimes, AGGREGATE_N)
 
-ax.set_title("Averaged over " + nRuns + " runs; timeout at " + timeI)
+xAxis = range(len(dpTimes))
+fig, ax = plt.subplots()
+ax.semilogy(xAxis, dpTimes, 'ko-', label='DP', mfc='none')
+ax.semilogy(xAxis, dpllTimes, 'bx-', label='DPLL')
+ax.semilogy(xAxis, cdclTimes, 'md-', label='CDCL', mfc='none')
+
+def xToNVar(x, pos):
+    return VARIABLES_USED[int(x)]
+
+fmt = ticker.FuncFormatter(xToNVar)
+ax.xaxis.set_major_formatter(fmt)
+
+ax.set_title("Averaged over " + nRuns + " runs; timeout at " + timeI +
+             "\n averaged over " + str(AGGREGATE_N) + " formulas of same size")
 
 legend = ax.legend(loc='right')
 plt.ylabel("Runtime [ms]")
+plt.xlabel("Number of variables")
 
 # print(dpTimes)
 # print(dpllTimes)
